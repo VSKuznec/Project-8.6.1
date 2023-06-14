8e2ad70d5faea668ef06f10da1c602e9e93d8e05
sevac <sevac@VS_KUZNEC>
2023-06-14 12:48:29 +0300

    Добавьте файлы проекта.

---
+++ Project 8.6/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Path = ("C:\\Repository");
+
+            try
+            {
+                DirectoryInfo Fs = new DirectoryInfo(Path);
+                TimeSpan timeSpan = TimeSpan.FromMinutes(30);
+
+                if (Fs.Exists)
+                {
+                    foreach (FileInfo file in Fs.GetFiles())
+                    {
+                        if (file.LastAccessTime + timeSpan < DateTime.Now)
+                        {
+                            file.Delete();
+                        }
+                    }
+
+                    foreach (DirectoryInfo SubFs in Fs.GetDirectories())
+                    {
+                        if (SubFs.Exists)
+                        {
+                            foreach (FileInfo ChildFile in SubFs.GetFiles())
+                            {
+                                if (ChildFile.LastAccessTime + timeSpan < DateTime.Now)
+                                {
+                                    ChildFile.Delete();
+                                }
+                            }
+
+                            Directory.Delete(SubFs.FullName, true);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error: Directory not found at path {0}", SubFs.FullName);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error: Folder not found at path {0}", Path);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
